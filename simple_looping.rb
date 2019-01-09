8c8c0f6b6f718144e66f32d6ca9be68fa3f6fb2b
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-09 13:33:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -41,6 +41,7 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
-  
+  for number_of_times in number_of_times
+  puts "Welcome to Flatiron School's Web Development Course!"
 end
 
